3894eaeb877c271755ca583bfb511d866265f8b6
SeongUk <84304379+wngktjd13@users.noreply.github.com>
2024-10-30 14:35:52 +0900

    [Silver III] Title: 삼각형 만들기, Time: 848 ms, Memory: 77808 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -8,26 +8,25 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
-        int result = 0;
+        // 삼각형의 성립 조건을 만족하지 않는 걍우 -1을 출력하도록 설정
+        int result = -1;
 
+        // 빨대의 길이를 arr 배열에 저장
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
 
+        // 삼각형의 성립 조건 : 가장 큰 변의 길이보다 나머지 두 변의 길이 합이 더 커야 함.
+        // 성립 조건을 만족할 때까지 반복
         while(n-3 >= 0){
             if (arr[n - 1] < arr[n - 2] + arr[n - 3]) {
-                result += arr[n - 1] + arr[n - 2] + arr[n - 3];
+                result = arr[n - 1] + arr[n - 2] + arr[n - 3];
                 break;
             }
             n--;
         }
-
-        if(result == 0){
-            System.out.println(-1);
-        }else{
-            System.out.println(result);
-        }
+        System.out.println(result);
     }
 }
