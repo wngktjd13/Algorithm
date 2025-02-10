c5d1bcab282861c088618afd4e1cbe6489de43a8
SeongUk <84304379+wngktjd13@users.noreply.github.com>
2025-02-11 00:28:07 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,18 +4,17 @@ import java.util.*;
 public class Main {
     static boolean[] visited;
     static int[][] graph;
-    static int count = 0;
-    static int node, line;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        node = Integer.parseInt(br.readLine());  // 컴퓨터 수
-        line = Integer.parseInt(br.readLine());  // 연결된 쌍 개수
+        int node = Integer.parseInt(br.readLine());  // 컴퓨터 개수
+        int line = Integer.parseInt(br.readLine());  // 연결된 쌍 개수
         
+        // 배열 선언
         graph = new int[node + 1][node + 1];  // 인접 행렬
-        visited = new boolean[node + 1];  // 방문 여부
-
+        visited = new boolean[node + 1];  // 방문 체크 배열
+        
         // 그래프 입력 받기
         for (int i = 0; i < line; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,20 +23,21 @@ public class Main {
             graph[a][b] = graph[b][a] = 1;  // 양방향 연결
         }
 
-        System.out.println(bfs(1));  // 1번 컴퓨터에서 시작
+        System.out.println(bfs(1, node));  // 1번 컴퓨터에서 시작
     }
 
-    public static int bfs(int start) {
+    public static int bfs(int start, int node) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
         visited[start] = true;
         int count = 0;
 
         while (!queue.isEmpty()) {
-            int cur = queue.poll();  // 현재 컴퓨터 꺼내기
+            int cur = queue.poll();  // 현재 감염된 컴퓨터 꺼내기
 
-            for (int i = 1; i <= node; i++) {  // 모든 컴퓨터 확인
-                if (graph[cur][i] == 1 && !visited[i]) {  // 연결되어 있고, 방문 안 했으면
+            // 모든 컴퓨터 확인
+            for (int i = 1; i < graph[cur].length; i++) {  
+                if (graph[cur][i] == 1 && !visited[i]) {  // 연결 & 미방문 체크
                     queue.add(i);
                     visited[i] = true;
                     count++;  // 감염된 컴퓨터 수 증가
